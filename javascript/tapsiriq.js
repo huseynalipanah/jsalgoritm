5faa57bda2b55ea664b3c38de521156fc47940da
huseynalipanahov <mi7US8AXF@gmail.com>
2023-10-25 12:19:37 +0400

    update

--- javascript/tapsiriq.js
+++ javascript/tapsiriq.js
@@ -1,35 +1,84 @@
+// 1 ci tapsiriq 
 // let eded = parseInt(prompt("Ededi Daxil Edin: "));
 
-// if (eded % 2 == 0) {
-//     alert("Bu eded 2-nin quvvetidir.");
-// } else {
-//     alert("Bu eded 2-nin quvveti deyil.");
+// if (n<1){
+//     console.log("quvvet deyil");
+    
+// }
+// while (n > 1) {
+//     if (n % 2 === 1){
+//         console.log("quvvet deyil");
+//         break;
+//     }
+//     n = n/2
+//     console.log("quvvetidir");
+    
 // }
 
+// son
 
-// 2-ci Tapsiriq 
+
+
+// 2-ci Tapsiriq
 // let x = parseInt(prompt("Birinci Ededi Daxil edin: "));
 // let y = parseInt(prompt("İkinci Ededi Daxil Edin: "));
 
 // console.log("Cavab: " + x**y);
 
+// son
 
 // 3-cu Tapsiriq
 
-let toplam = 0;
+// let cem = 0;
+// let yuz = 0;
+// let min = 0;
+// for (let i = 0; i < 100; i++) {
+//   if (i % 11 === 0) yuz = yuz + i;
+// }
+// for (let i = 0; i < 1000; i++) {
+//   if (i % 111 === 0) min = min + i;
+// }
+// alert(min + yuz);
+// son
 
+// 4 cu tapsiriq
+// const arr = [2, 4, 5, 32, 43, 56, 23, 12, 66];
+// let max = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max && arr[i] % 2 === 0) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+// son
 
-    for (let j = 0; j < sayi.length - 1; j++) {
-        if (sayi[j] !== sayi[j + 1]) {
-            ayniRakam = false;
-            break;
-        }
-    }
+// 5 ci tapsiriq
+// const arr = [ 3 , 2 , 43 , 54 , 12 , 32 , 56 , 67 , 85, 2]
+// let min = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min && arr[i] % 2 === 1) {
+//       min = arr[i];
+//     }
+//   }
+//   console.log(min);
+// son
+// 7 ci tapsiriq
+// const arr = [ 3 , 2 , 43 , 54 , 12 , 32 , 56 , 67 , 85, 2]
+//  let max = arr[0];
+//  let min = arr[0];
+//  for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max){
+//         max = arr[i];
+//     }
+//  }
+//  console.log("Boyuk: " + max);
+//  for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min){
+//         min = arr[i];
+//     }
+//  }
+//  console.log("Kicik: " + min);
+//  son
 
-    if (ayniRakam) {
-        toplam += i;
-    }
-
-console.log("Toplam: " + toplam);
 
 
